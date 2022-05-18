6ed18acfee5f951cdb53fb3a6efbbb9acbb4a769
Nivasan <58383171+NivasanV@users.noreply.github.com>
2022-05-18 11:57:09 +0530

    multiple seat book
    we can book multiple seats at a time

--- src/theater/Main.java
+++ src/theater/Main.java
@@ -26,25 +26,35 @@ public class Main {
 	}
 	static void book(String k) {
 		k = k.toUpperCase();
-		char key[] = k.toCharArray();
-		int col = key[0];
-		int row =0;
-		if(key.length == 3) row = 58;
-		else row = key[1];
-		arr[row-49][col-65] = '*';
+		String str[] = k.split(",");
+		for(int i=0; i<str.length; i++) { // for multiple seat booking
+			String s = str[i];
+			char key[] = s.toCharArray();
+			int col = key[0];
+			int row =0;
+			if(key.length == 3) row = 58;
+			else row = key[1];
+			arr[row-49][col-65] = '*';
+		}
+//		char key[] = k.toCharArray();
+//		int col = key[0];
+//		int row =0;
+//		if(key.length == 3) row = 58;
+//		else row = key[1];
+//		arr[row-49][col-65] = '*';
 		list();
-		System.out.println("Your seat is booked a " +k);
+		System.out.println("Your seats is booked at " +k);
 	}
 	
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
 			int n = 0;
 			do {
-				System.out.print("1.check  2.book  3.exit  :");
+				System.out.print("1.check  2.book  0.exit  :");
 				n = sc.nextInt();
 				switch(n) {
 				case 1: list(); break;
-				case 2: System.out.print("Enter the seat : ");
+				case 2: System.out.print("Enter the seats to book : ");
 				 		String str = sc.next();
 				 		book(str); break;
 				case 0: System.out.print("Bey");
